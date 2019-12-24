531fc1578c787ba382b6575c83d568f568fac978
guylaroche237 <guylarochefeu@gmail.com>
2019-12-24 17:50:40 +0100

    first commit du serveur

---
+++ src/test/java/com/sdze/gst/guy/ServeurGestionStockProduitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sdze.gst.guy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServeurGestionStockProduitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
